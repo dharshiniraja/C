3ccb9fed4a3b8f6222d0bc6da57e20021b812ba5
dharshiniraja <47824835+dharshiniraja@users.noreply.github.com>
2019-06-30 13:29:44 +0530

    Update diagonal_pattern.c

--- pro/diagonal_pattern.c
+++ pro/diagonal_pattern.c
@@ -11,3 +11,26 @@ int main()
      printf("%-*.*s%*s\n",len,i+1,str,len,str+len-i-1);
    return 0;
 }
+
+Input:
+ dharshini
+Output:
+
+dharshinidharshini                                                                                                                      
+dharshin  harshini                                                                                                                      
+dharshi    arshini                                                                                                                      
+dharsh      rshini                                                                                                                      
+dhars        shini                                                                                                                      
+dhar          hini                                                                                                                      
+dha            ini                                                                                                                      
+dh              ni                                                                                                                      
+d                i                                                                                                                      
+d                i                                                                                                                      
+dh              ni                                                                                                                      
+dha            ini                                                                                                                      
+dhar          hini                                                                                                                      
+dhars        shini                                                                                                                      
+dharsh      rshini                                                                                                                      
+dharshi    arshini                                                                                                                      
+dharshin  harshini                                                                                                                      
+dharshinidharshini
