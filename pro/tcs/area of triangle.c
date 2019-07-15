dfa73e7708d83212afbb38794cae7f2da0a1ad5a
dharshiniraja <47824835+dharshiniraja@users.noreply.github.com>
2019-07-15 21:12:02 +0530

    Create area of triangle.c

---
+++ pro/tcs/area of triangle.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<math.h>
+int main(){
+  double a,b,c;
+  scanf("%lf %lf %lf",&a,&b,&c);
+  double p=0,s=0;
+  p=(a+b+c)/2;
+  s=sqrt(p*(p-a)*(p-b)*(p-c));
+  print(".2lf",s);
+  return 0;
+  }
+  
+  
+  Input:
+  2 3 4
+  
+  Output:
+  2.90
+  
