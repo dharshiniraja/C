a5853338eecc6bf21f3879788e894c1d8e53be69
dharshiniraja <47824835+dharshiniraja@users.noreply.github.com>
2019-07-18 16:07:37 +0530

    Update pattern.c

--- pro/patterns/pattern.c
+++ pro/patterns/pattern.c
@@ -16,24 +16,7 @@ int main() {
 }
 
 
-#include <stdio.h>
 
-int main() {
-	int n;
-	int c=0;
-	scanf("%d",&n);
-	for(int i=0;i<n;i++){
-	    for(int j=0;j<=i;j++)
-	    {
-         if(i==j)
-	        printf("%3d\n",++c);
-         else
-           printF("%3d *,++c);
-	     }
-	    printf("\b \n");
-	}
-	return 0;
-}
 
 Input:
 5
