68f7307adf9884ce54e473c4b2d22fa9efd58ab9
dharshiniraja <47824835+dharshiniraja@users.noreply.github.com>
2019-08-06 14:48:31 +0530

    Create greatest of 4 nos.c

---
+++ pro/greatest of 4 nos.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    while(n>0)
+    {
+        int n1,n2,n3,n4;
+        scanf("%d %d %d %d",&n1,&n2,&n3,&n4);
+        if(n1>n2)
+        {
+            if(n1>n3)
+            {
+                if(n1>n4)
+                {
+                   printf("Largest of %d,%d,%d,%d is %d",n1,n2,n3,n4,n1); 
+                }
+                else
+                {
+                   printf("Largest of %d,%d,%d,%d is %d",n1,n2,n3,n4,n4); 
+                }
+            }
+        }
+        else if(n2>n3)
+        {
+            if(n2>n4)
+            {
+                printf("Largest of %d,%d,%d,%d is %d",n1,n2,n3,n4,n2);
+            }
+            else
+            {
+                printf("Largest of %d,%d,%d,%d is %d",n1,n2,n3,n4,n4);
+            }
+        }
+        else if(n3>n4)
+        {
+            printf("Largest of %d,%d,%d,%d is %d",n1,n2,n3,n4,n3);
+        }
+        else
+        {
+            printf("Largest of %d,%d,%d,%d is %d",n1,n2,n3,n4,n4);
+        }
+        n=n-1;
+    }
+
+    return 0;
+}
